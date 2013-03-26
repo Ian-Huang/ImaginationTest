f4b37a76224db1b3dd4d15ecb16b4d951d250177
Ian-Huang <dk781020@hotmail.com>
2013-03-26 20:46:29 +0800

    大更新:增加不同的layout來支持不同解析度的螢幕

--- gen/com/example/imaginationtest/R.java
+++ gen/com/example/imaginationtest/R.java
@@ -10,125 +10,134 @@ package com.example.imaginationtest;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int RedColor=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
+ Default screen margins, per the Android Design guidelines. 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
-        public static final int font_size=0x7f040002;
+        public static final int font_size=0x7f040003;
+        public static final int font_size_small=0x7f040002;
+        public static final int font_size_title=0x7f040004;
     }
     public static final class drawable {
         public static final int homepage_picture=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int radiobutton=0x7f020002;
+        public static final int radiobutton_off=0x7f020003;
+        public static final int radiobutton_on=0x7f020004;
     }
     public static final class id {
-        public static final int Act1Timer_textView=0x7f080000;
-        public static final int Act1_NextActivityButton=0x7f080001;
-        public static final int Act1answer_EditText01=0x7f080002;
-        public static final int Act1answer_EditText02=0x7f080003;
-        public static final int Act1answer_EditText03=0x7f080004;
-        public static final int Act1answer_EditText04=0x7f080005;
-        public static final int Act1answer_EditText05=0x7f080006;
-        public static final int Act1answer_EditText06=0x7f080007;
-        public static final int Act1answer_EditText07=0x7f080008;
-        public static final int Act1answer_EditText08=0x7f080009;
-        public static final int Act1answer_EditText09=0x7f08000a;
-        public static final int Act1answer_EditText10=0x7f08000b;
-        public static final int Act1answer_EditText11=0x7f08000c;
-        public static final int Act1answer_EditText12=0x7f08000d;
-        public static final int Act1answer_EditText13=0x7f08000e;
-        public static final int Act1answer_EditText14=0x7f08000f;
-        public static final int Act1answer_EditText15=0x7f080010;
-        public static final int Act1answer_EditText16=0x7f080011;
-        public static final int Act1answer_EditText17=0x7f080012;
-        public static final int Act1answer_EditText18=0x7f080013;
-        public static final int Act1answer_EditText19=0x7f080014;
-        public static final int Act1answer_EditText20=0x7f080015;
-        public static final int Act1answer_EditText21=0x7f080016;
-        public static final int Act1answer_EditText22=0x7f080017;
-        public static final int Act1answer_EditText23=0x7f080018;
-        public static final int Act1answer_EditText24=0x7f080019;
-        public static final int Act1answer_EditText25=0x7f08001a;
-        public static final int Act1answer_EditText26=0x7f08001b;
-        public static final int Act1answer_EditText27=0x7f08001c;
-        public static final int Act1answer_EditText28=0x7f08001d;
-        public static final int Act1answer_EditText29=0x7f08001e;
-        public static final int Act1answer_EditText30=0x7f08001f;
-        public static final int Act1answer_EditText31=0x7f080020;
-        public static final int Act1answer_EditText32=0x7f080021;
-        public static final int Act1answer_EditText33=0x7f080022;
-        public static final int Act1answer_EditText34=0x7f080023;
-        public static final int Act1answer_EditText35=0x7f080024;
-        public static final int Act1answer_EditText36=0x7f080025;
-        public static final int Act1answer_EditText37=0x7f080026;
-        public static final int Act1answer_EditText38=0x7f080027;
-        public static final int Act1answer_EditText39=0x7f080028;
-        public static final int Act1answer_EditText40=0x7f080029;
-        public static final int Act1answer_EditText41=0x7f08002a;
-        public static final int Act1answer_EditText42=0x7f08002b;
-        public static final int Act1answer_EditText43=0x7f08002c;
-        public static final int Act1answer_EditText44=0x7f08002d;
-        public static final int Act1answer_EditText45=0x7f08002e;
-        public static final int Act2Timer_textView=0x7f080031;
-        public static final int Act2_NextActivityButton=0x7f080032;
-        public static final int Act2answer_EditText01=0x7f080033;
-        public static final int Act2answer_EditText02=0x7f080034;
-        public static final int Act2answer_EditText03=0x7f080035;
-        public static final int Act2answer_EditText04=0x7f080036;
-        public static final int Act2answer_EditText05=0x7f080037;
-        public static final int Act2answer_EditText06=0x7f080038;
-        public static final int Act2answer_EditText07=0x7f080039;
-        public static final int Act2answer_EditText08=0x7f08003a;
-        public static final int Act2answer_EditText09=0x7f08003b;
-        public static final int Act2answer_EditText10=0x7f08003c;
-        public static final int Act2answer_EditText11=0x7f08003d;
-        public static final int Act2answer_EditText12=0x7f08003e;
-        public static final int Act2answer_EditText13=0x7f08003f;
-        public static final int Act2answer_EditText14=0x7f080040;
-        public static final int Act2answer_EditText15=0x7f080041;
-        public static final int Act2answer_EditText16=0x7f080042;
-        public static final int Act2answer_EditText17=0x7f080043;
-        public static final int Act2answer_EditText18=0x7f080044;
-        public static final int Act2answer_EditText19=0x7f080045;
-        public static final int Act2answer_EditText20=0x7f080046;
-        public static final int Act2answer_EditText21=0x7f080047;
-        public static final int Act2answer_EditText22=0x7f080048;
-        public static final int Act2answer_EditText23=0x7f080049;
-        public static final int Act2answer_EditText24=0x7f08004a;
-        public static final int Act2answer_EditText25=0x7f08004b;
-        public static final int Act2answer_EditText26=0x7f08004c;
-        public static final int Act2answer_EditText27=0x7f08004d;
-        public static final int Act2answer_EditText28=0x7f08004e;
-        public static final int Act2answer_EditText29=0x7f08004f;
-        public static final int Act2answer_EditText30=0x7f080050;
-        public static final int Act2answer_EditText31=0x7f080051;
-        public static final int Act2answer_EditText32=0x7f080052;
-        public static final int Act2answer_EditText33=0x7f080053;
-        public static final int Act2answer_EditText34=0x7f080054;
-        public static final int Act2answer_EditText35=0x7f080055;
-        public static final int Act2answer_EditText36=0x7f080056;
-        public static final int Act2answer_EditText37=0x7f080057;
-        public static final int Act2answer_EditText38=0x7f080058;
-        public static final int Act2answer_EditText39=0x7f080059;
-        public static final int Act2answer_EditText40=0x7f08005a;
-        public static final int Act2answer_EditText41=0x7f08005b;
-        public static final int Birthday_testView=0x7f080063;
-        public static final int GradeYear_editText=0x7f080061;
-        public static final int HomePage_ImageButton=0x7f08005d;
-        public static final int SchoolName_editText=0x7f080060;
-        public static final int SetBirthday_button=0x7f080062;
-        public static final int StartTest_button=0x7f080065;
-        public static final int TestDay_testView=0x7f080064;
-        public static final int UserName_editText=0x7f08005e;
-        public static final int act1Introduction_StartTestButton=0x7f080030;
-        public static final int act2Introduction_StartTestButton=0x7f08005c;
-        public static final int action_settings=0x7f080066;
-        public static final int radioGroup_sex=0x7f08005f;
-        public static final int scrollView1=0x7f08002f;
+        public static final int Act1Timer_textView=0x7f090000;
+        public static final int Act1_NextActivityButton=0x7f090001;
+        public static final int Act1answer_EditText01=0x7f090002;
+        public static final int Act1answer_EditText02=0x7f090003;
+        public static final int Act1answer_EditText03=0x7f090004;
+        public static final int Act1answer_EditText04=0x7f090005;
+        public static final int Act1answer_EditText05=0x7f090006;
+        public static final int Act1answer_EditText06=0x7f090007;
+        public static final int Act1answer_EditText07=0x7f090008;
+        public static final int Act1answer_EditText08=0x7f090009;
+        public static final int Act1answer_EditText09=0x7f09000a;
+        public static final int Act1answer_EditText10=0x7f09000b;
+        public static final int Act1answer_EditText11=0x7f09000c;
+        public static final int Act1answer_EditText12=0x7f09000d;
+        public static final int Act1answer_EditText13=0x7f09000e;
+        public static final int Act1answer_EditText14=0x7f09000f;
+        public static final int Act1answer_EditText15=0x7f090010;
+        public static final int Act1answer_EditText16=0x7f090011;
+        public static final int Act1answer_EditText17=0x7f090012;
+        public static final int Act1answer_EditText18=0x7f090013;
+        public static final int Act1answer_EditText19=0x7f090014;
+        public static final int Act1answer_EditText20=0x7f090015;
+        public static final int Act1answer_EditText21=0x7f090016;
+        public static final int Act1answer_EditText22=0x7f090017;
+        public static final int Act1answer_EditText23=0x7f090018;
+        public static final int Act1answer_EditText24=0x7f090019;
+        public static final int Act1answer_EditText25=0x7f09001a;
+        public static final int Act1answer_EditText26=0x7f09001b;
+        public static final int Act1answer_EditText27=0x7f09001c;
+        public static final int Act1answer_EditText28=0x7f09001d;
+        public static final int Act1answer_EditText29=0x7f09001e;
+        public static final int Act1answer_EditText30=0x7f09001f;
+        public static final int Act1answer_EditText31=0x7f090020;
+        public static final int Act1answer_EditText32=0x7f090021;
+        public static final int Act1answer_EditText33=0x7f090022;
+        public static final int Act1answer_EditText34=0x7f090023;
+        public static final int Act1answer_EditText35=0x7f090024;
+        public static final int Act1answer_EditText36=0x7f090025;
+        public static final int Act1answer_EditText37=0x7f090026;
+        public static final int Act1answer_EditText38=0x7f090027;
+        public static final int Act1answer_EditText39=0x7f090028;
+        public static final int Act1answer_EditText40=0x7f090029;
+        public static final int Act1answer_EditText41=0x7f09002a;
+        public static final int Act1answer_EditText42=0x7f09002b;
+        public static final int Act1answer_EditText43=0x7f09002c;
+        public static final int Act1answer_EditText44=0x7f09002d;
+        public static final int Act1answer_EditText45=0x7f09002e;
+        public static final int Act2Timer_textView=0x7f090031;
+        public static final int Act2_NextActivityButton=0x7f090032;
+        public static final int Act2answer_EditText01=0x7f090033;
+        public static final int Act2answer_EditText02=0x7f090034;
+        public static final int Act2answer_EditText03=0x7f090035;
+        public static final int Act2answer_EditText04=0x7f090036;
+        public static final int Act2answer_EditText05=0x7f090037;
+        public static final int Act2answer_EditText06=0x7f090038;
+        public static final int Act2answer_EditText07=0x7f090039;
+        public static final int Act2answer_EditText08=0x7f09003a;
+        public static final int Act2answer_EditText09=0x7f09003b;
+        public static final int Act2answer_EditText10=0x7f09003c;
+        public static final int Act2answer_EditText11=0x7f09003d;
+        public static final int Act2answer_EditText12=0x7f09003e;
+        public static final int Act2answer_EditText13=0x7f09003f;
+        public static final int Act2answer_EditText14=0x7f090040;
+        public static final int Act2answer_EditText15=0x7f090041;
+        public static final int Act2answer_EditText16=0x7f090042;
+        public static final int Act2answer_EditText17=0x7f090043;
+        public static final int Act2answer_EditText18=0x7f090044;
+        public static final int Act2answer_EditText19=0x7f090045;
+        public static final int Act2answer_EditText20=0x7f090046;
+        public static final int Act2answer_EditText21=0x7f090047;
+        public static final int Act2answer_EditText22=0x7f090048;
+        public static final int Act2answer_EditText23=0x7f090049;
+        public static final int Act2answer_EditText24=0x7f09004a;
+        public static final int Act2answer_EditText25=0x7f09004b;
+        public static final int Act2answer_EditText26=0x7f09004c;
+        public static final int Act2answer_EditText27=0x7f09004d;
+        public static final int Act2answer_EditText28=0x7f09004e;
+        public static final int Act2answer_EditText29=0x7f09004f;
+        public static final int Act2answer_EditText30=0x7f090050;
+        public static final int Act2answer_EditText31=0x7f090051;
+        public static final int Act2answer_EditText32=0x7f090052;
+        public static final int Act2answer_EditText33=0x7f090053;
+        public static final int Act2answer_EditText34=0x7f090054;
+        public static final int Act2answer_EditText35=0x7f090055;
+        public static final int Act2answer_EditText36=0x7f090056;
+        public static final int Act2answer_EditText37=0x7f090057;
+        public static final int Act2answer_EditText38=0x7f090058;
+        public static final int Act2answer_EditText39=0x7f090059;
+        public static final int Act2answer_EditText40=0x7f09005a;
+        public static final int Act2answer_EditText41=0x7f09005b;
+        public static final int Birthday_testView=0x7f090065;
+        public static final int GradeYear_editText=0x7f090063;
+        public static final int HomePage_ImageButton=0x7f09005f;
+        public static final int SchoolName_editText=0x7f090062;
+        public static final int SetBirthday_button=0x7f090064;
+        public static final int StartTest_button=0x7f090067;
+        public static final int TestDay_testView=0x7f090066;
+        public static final int UserName_editText=0x7f090060;
+        public static final int act1Introduction_StartTestButton=0x7f090030;
+        public static final int act2Introduction_StartTestButton=0x7f09005c;
+        public static final int action_settings=0x7f090068;
+        public static final int radioGroup1=0x7f09005d;
+        public static final int radioGroup_sex=0x7f090061;
+        public static final int scrollView1=0x7f09002f;
+        public static final int textView1=0x7f09005e;
     }
     public static final class layout {
         public static final int activity1_page=0x7f030000;
@@ -140,12 +149,12 @@ public final class R {
         public static final int personalinformation_page=0x7f030006;
     }
     public static final class menu {
-        public static final int home_page=0x7f070000;
+        public static final int home_page=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -157,11 +166,44 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
