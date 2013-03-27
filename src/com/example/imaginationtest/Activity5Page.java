937ff026037ba07e14221b9ae09a76a40211061c
Ian-Huang <dk781020@hotmail.com>
2013-03-27 23:07:47 +0800

    活動5功能 未完成

--- src/com/example/imaginationtest/Activity5Page.java
+++ src/com/example/imaginationtest/Activity5Page.java
@@ -2,14 +2,61 @@ package com.example.imaginationtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Activity5Page extends Activity {
+
+	private RadioButton[][] radioButtonCollection = new RadioButton[15][6];
+	public static int currentGroup = 0;
+	public static int currentIndex = 0;
+
+	TextView testViewxstr;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity5_page);
 
+		this.Init();
+	}
+
+	void Init() {
+		testViewxstr = (TextView) findViewById(R.id.act5_testI);
+		int count = 0;
+		for (RadioButton[] element : radioButtonCollection) {
+			currentIndex = 0;
+			for (RadioButton radioButton : element) {
+				radioButton = (RadioButton) findViewById(R.id.act5_Radiobtn_answer01_1
+						+ count);
+
+				radioButton
+						.setOnClickListener(new RadioButton.OnClickListener() {
+
+							public int group = Activity5Page.currentGroup;
+							public int index = Activity5Page.currentIndex;
+
+							@Override
+							public void onClick(View v) {
+								 testViewxstr.setText(Integer
+								 .toString(radioButtonCollection[group][index].getId()));
+//								for (RadioButton radioButton : radioButtonCollection[group]) {
+//									radioButton.setChecked(false);
+//								}
+//
+//								radioButtonCollection[group][index]
+//										.setChecked(false);
+							}
+						});
+
+				count++;
+				currentIndex++;
+			}
+			currentGroup++;
+		}
 
 	}
+
 }
