403a9a16915e2c0f6f8dd877ff2dcb448eac74df
Ian-Huang <dk781020@hotmail.com>
2013-04-18 01:34:41 +0800

    Update OpenGL Learn

--- src/com/example/imaginationtest/OpenGL/MySurfaceView.java
+++ src/com/example/imaginationtest/OpenGL/MySurfaceView.java
@@ -1,11 +1,12 @@
 package com.example.imaginationtest.OpenGL;
 
+import java.nio.ByteBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import com.example.imaginationtest.Activity3Page;
 import com.example.imaginationtest.R;
-
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
@@ -18,8 +19,6 @@ public class MySurfaceView extends GLSurfaceView implements Renderer {
 
 	private float myPreviousY;
 	private float myPreviousX;
-
-	
 
 	public MySurfaceView(Context context) {
 		super(context);
@@ -36,9 +35,7 @@ public class MySurfaceView extends GLSurfaceView implements Renderer {
 		float x = event.getX();
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_MOVE:
-			
-			
-			
+
 			float deltaY = y - this.myPreviousY;
 			float deltaX = x - this.myPreviousX;
 			Activity3Page.TestPosX.setText(String.valueOf(deltaX));
@@ -56,22 +53,36 @@ public class MySurfaceView extends GLSurfaceView implements Renderer {
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		// TODO Auto-generated method stub
-		gl.glEnable(GL10.GL_CULL_FACE);
-		gl.glShadeModel(GL10.GL_SMOOTH);
-		gl.glFrontFace(GL10.GL_CCW);
+		gl.glEnable(GL10.GL_CULL_FACE); // 打開背面剪裁
+		gl.glShadeModel(GL10.GL_SMOOTH); // 著色模型:平滑著色
+		gl.glFrontFace(GL10.GL_CCW); // 捲繞順序:逆時針為正面
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT); // 清除暫存
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW); // 當前矩陣:模式矩陣(操作物體時使用模型矩陣，如平移、旋轉、縮放等。)
+		gl.glLoadIdentity(); // 當前矩陣為單位矩陣
+
+		gl.glTranslatef(0, 0, -2.0f); // 位移
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		// TODO Auto-generated method stub
 
+		gl.glViewport(0, 0, width, height); // 視窗大小和位置
+		gl.glMatrixMode(GL10.GL_PROJECTION);// 投影矩陣(設置觀察物體的方式時，選擇投影矩陣)
+		gl.glLoadIdentity(); // 當前矩陣為單位矩陣
+
+		float ratio = width / height;
+		gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);// 投影模式
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		// TODO Auto-generated method stub
-		
+		gl.glDisable(GL10.GL_DITHER); // 關閉抗抖動(非必要:若機器的分辨率已經相當高)
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_DONT_CARE);
 
+		gl.glClearColor(0, 0, 0, 0); // 背景色
+		gl.glEnable(GL10.GL_DEPTH_TEST); // 啟動深度檢測(沒有看到的面就不會被畫)
 	}
-
 }
