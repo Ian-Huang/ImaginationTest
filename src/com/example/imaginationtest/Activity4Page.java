a0a76bf88f93c27ca122c33f6e713a7fa005177e
ShinMin <joey514111@hotmail.com>
2013-04-26 01:23:52 +0800

    活動4  版面與按鈕....努力

---
+++ src/com/example/imaginationtest/Activity4Page.java
@@ -0,0 +1,15 @@
+package com.example.imaginationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity4Page extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity4_page);
+	}
+
+}
