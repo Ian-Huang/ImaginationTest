155d5bc8c0a0df503c9208ca4a1c50a4fff25ee0
新民 <joey514111@hotmail.com>
2013-04-26 16:00:41 +0800

    Act4 存檔按鈕(code還沒寫)

--- src/com/example/imaginationtest/Activity4Page.java
+++ src/com/example/imaginationtest/Activity4Page.java
@@ -2,14 +2,39 @@ package com.example.imaginationtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity4Page extends Activity {
 
+	private Button Act4_ClearButton;
+	private Button Act4_EraserButton;
+	private Button Act4_NextPageButton;
+	private Button Act4_PreviousPageButton;
+	private Button Act4_RedoButton;
+	private Button Act4_SaveFileButton;
+	private Button Act4_UndoButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity4_page);
+		
+		Init();
+	
+	}
+	
+	void Init()
+	{
+		// ¶s¿…Button
+		this.Act4_SaveFileButton = (Button) findViewById(R.id.Act4_SaveFileButton);
+		this.Act4_SaveFileButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 	}
 
 }
